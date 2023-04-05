3f6c6cd3952f13e1e2cf92286f13b84597a24643
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2023-04-05 16:29:26 +0200

    small changes

--- delete_save.bat
+++ delete_save.bat
@@ -1,2 +1,5 @@
+@echo off
 del "cmake-build-debug-visual-studio\src\Save1.json"
 del "cmake-build-release-visual-studio\src\Save1.json"
+del "cmake-build-debug\src\Save1.json"
+del "cmake-build-release\src\Save1.json"
