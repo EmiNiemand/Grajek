8b27afaad09859c8ef5172be3c6ef3cce9e25082
Kamiloxxo <115363507+Kamiloxxo@users.noreply.github.com>
2023-04-21 17:45:49 +0200

    bug fixes

--- src/src/Components/Scripts/ShopMenu.cpp
+++ src/src/Components/Scripts/ShopMenu.cpp
@@ -11,24 +11,47 @@ ShopMenu::~ShopMenu() {}
 
 void ShopMenu::ShowMenu() {
     parent->EnableSelfAndChildren();
-    activeButton = GloomEngine::GetInstance()->FindGameObjectWithName("FourthInstrument")->GetComponent<Button>();
-    activeButton->isActive = true;
+    if (GloomEngine::GetInstance()->FindGameObjectWithName("Shop")->children.size() > 1) {
+        activeButton = GloomEngine::GetInstance()->FindGameObjectWithName("Shop")->children.begin()->second->GetComponent<Button>();
+        activeButton->isActive = true;
+    }
 }
 
 void ShopMenu::OnClick() {
+    if (GloomEngine::GetInstance()->FindGameObjectWithName("Shop")->children.size() < 2) return;
+    bool bought;
     auto playerManager = GloomEngine::GetInstance()->FindGameObjectWithName("Player")->GetComponent<PlayerManager>();
     if (activeButton->GetParent()->GetName() == "FirstInstrument") {
-        if(playerManager->BuyInstrument(10, Prefab::GetInstrument(InstrumentName::Drums))) {
-            // TODO delete button with instrument
-            std::shared_ptr<Button> temp = activeButton->previousButton;
-            activeButton->previousButton->nextButton = activeButton->nextButton;
-            activeButton->nextButton->previousButton = temp;
-            GameObject::Destroy(activeButton->GetParent());
-//            GameObject::Destroy(GloomEngine::GetInstance()->FindGameObjectWithName("ui"));
-            activeButton = activeButton->nextButton;
-            spdlog::info("[SM] Bought first instrument (percussion)!");
-        } else {
-            spdlog::info("[SM] Not enough money for first instrument (percussion)");
-        }
+        if (playerManager->BuyInstrument(10, Prefab::GetInstrument(InstrumentName::Drums)))
+            bought = true;
+        else
+            bought = false;
+    } else if (activeButton->GetParent()->GetName() == "SecondInstrument") {
+        if (playerManager->BuyInstrument(15, Prefab::GetInstrument(InstrumentName::Trumpet)))
+            bought = true;
+        else
+            bought = false;
+    } else if (activeButton->GetParent()->GetName() == "ThirdInstrument") {
+        if (playerManager->BuyInstrument(20, Prefab::GetInstrument(InstrumentName::Launchpad)))
+            bought = true;
+        else
+            bought = false;
+    } else if (activeButton->GetParent()->GetName() == "FourthInstrument") {
+        if (playerManager->BuyInstrument(25, Prefab::GetInstrument(InstrumentName::Guitar)))
+            bought = true;
+        else
+            bought = false;
+    }
+
+    if (bought) {
+        spdlog::info("[SM] Bought first instrument (percussion)!");
+        std::shared_ptr<Button> temp = activeButton->previousButton;
+        activeButton->previousButton->nextButton = activeButton->nextButton;
+        activeButton->nextButton->previousButton = temp;
+        GameObject::Destroy(activeButton->GetParent());
+        activeButton = temp;
+        activeButton->isActive = true;
+    } else {
+        spdlog::info("[SM] Not enough money for first instrument (percussion)");
     }
 }
