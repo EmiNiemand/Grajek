37f409a4465d599e3af0f88d9c3f036afd50ed79
Kamiloxxo <115363507+Kamiloxxo@users.noreply.github.com>
2023-05-17 00:06:47 +0200

    Add SavePoint model

--- src/src/GameObjectsAndPrefabs/Prefabs/SavePoint.cpp
+++ src/src/GameObjectsAndPrefabs/Prefabs/SavePoint.cpp
@@ -3,6 +3,7 @@
 #include "Components/PhysicsAndColliders/BoxCollider.h"
 #include "Components/Scripts/SavePointTrigger.h"
 #include "Components/UI/Image.h"
+#include "Components/Animations/GameObjectAnimator.h"
 
 SavePoint::SavePoint(const std::string &name, int id, const std::shared_ptr<GameObject> &parent, Tags tag) : Prefab(
         name, id, parent, tag) {
@@ -13,9 +14,14 @@ SavePoint::~SavePoint() = default;
 
 std::shared_ptr<GameObject> SavePoint::Create() {
     auto savePoint = shared_from_this();
-    savePoint->AddComponent<Renderer>()->LoadModel("texturedModels/hydrant.obj");
-    savePoint->AddComponent<BoxCollider>()->SetOffset({3.25, 0.5, 0});
+    savePoint->AddComponent<Renderer>()->LoadModel("SavePoint.obj");
+    savePoint->AddComponent<BoxCollider>()->SetOffset({0, 0, 0});
     savePoint->GetComponent<BoxCollider>()->SetSize({0.5, 1, 0.5});
+    std::shared_ptr<GameObject> s = GameObject::Instantiate("S", savePoint);
+    s->AddComponent<Renderer>()->LoadModel("S.obj");
+    s->AddComponent<GameObjectAnimator>()->Setup(s->transform, {
+            {AnimatedProperty::Rotation, glm::vec3(0.0f, -360.0f, 0.0f), 5.0f},
+    }, true);
 
     auto savePointTrigger = GameObject::Instantiate("SavePointTrigger", savePoint);
     savePointTrigger->AddComponent<BoxCollider>()->SetOffset({3.25, 0.5, 0});
