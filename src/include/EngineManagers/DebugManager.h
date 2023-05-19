7f117c61e79f6e43146fcc4ce2d546cea948d6fa
Mateusz Dargiel <236515@edu.p.lodz.pl>
2023-05-19 12:46:26 +0200

    GameObjectData, Object creator Ui, basic creator logic

--- src/include/EngineManagers/DebugManager.h
+++ src/include/EngineManagers/DebugManager.h
@@ -6,6 +6,7 @@
 #define GLOOMENGINE_DEBUGMANAGER_H
 #include <GloomEngine.h>
 #include <GameObjectsAndPrefabs/GameObject.h>
+#include "LowLevelClasses/GameObjectData.h"
 
 
 class DebugManager {
@@ -17,16 +18,18 @@ private:
 	//flag to check if position of the GameObject was already extracted. 
 	bool transformExtracted;
 	std::shared_ptr<GameObject> selected;
-
+    bool isNewObjectBeingHeld;
+    std::shared_ptr<GameObjectData> gameObjectHolder;
 
 	void ProcessChildren(std::shared_ptr<GameObject> gameObject);
 	//Conversion from vec3 to float[3] for use in imgui.
-	void ExtractVec3ToFloat3(glm::vec3 input,float* output);
+	static void ExtractVec3ToFloat3(glm::vec3 input,float* output);
 	//Conversion from float[3] to vec3 to set it in the object.
-	glm::vec3 InjectFloat3IntoVec3(float * input);
+	static glm::vec3 InjectFloat3IntoVec3(float * input);
 	//Function to display RAM and CPU usage.
 	void DisplaySystemInfo();
 	void SaveMenu();
+    void ObjectCreator();
 public:
 	DebugManager(DebugManager& other) = delete;
 	void operator=(const DebugManager&) = delete;
@@ -37,6 +40,8 @@ public:
 	void Initialize(GLFWwindow* window, const char* glsl_version);
 	void Render();
     void Free() const;
+
+    void CreateGameObjectFromData(std::shared_ptr<GameObjectData>);
 };
 
 #endif //GLOOMENGINE_DEBUGMANAGER_H
---
+++ src/src/LowLevelClasses/GameObjectData.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mateusz on 19.05.2023.
+//
+
+#include "LowLevelClasses/GameObjectData.h"
