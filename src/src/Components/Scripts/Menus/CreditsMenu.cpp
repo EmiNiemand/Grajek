69fc72877802bba937b7dbd75cd9d01ee5f3b7e3
Kamiloxxo <115363507+Kamiloxxo@users.noreply.github.com>
2023-06-27 17:22:30 +0200

    Change Credits Menu

--- src/src/Components/Scripts/Menus/CreditsMenu.cpp
+++ src/src/Components/Scripts/Menus/CreditsMenu.cpp
@@ -1,10 +1,25 @@
 #include "Components/Scripts/Menus/CreditsMenu.h"
 #include "GameObjectsAndPrefabs/GameObject.h"
+#include "Components/UI/Button.h"
+#include "Components/Scripts/Menus/MainMenuManager.h"
+#include "Components/Scripts/Menus/MainMenu.h"
 
 CreditsMenu::CreditsMenu(const std::shared_ptr<GameObject> &parent, int id) : Menu(parent, id) {}
 
 CreditsMenu::~CreditsMenu() = default;
 
 bool CreditsMenu::ShowMenu() {
-    return Menu::ShowMenu();
+    if(!Menu::ShowMenu()) return false;
+
+    activeButton = GloomEngine::GetInstance()->FindGameObjectWithName("CreditsBackButton")->GetComponent<Button>();
+    activeButton->isActive = true;
+    return true;
+}
+
+void CreditsMenu::OnClick() {
+    auto mainMenuManager = GloomEngine::GetInstance()->FindGameObjectWithName("MainMenuManager")->GetComponent<MainMenuManager>();
+    HideMenu();
+    mainMenuManager->activeMenu = mainMenuManager->mainMenu;
+    mainMenuManager->mainMenu->ShowMenu();
+    Menu::OnClick();
 }
